a4fdfec866a7a268eb4c260541f9c5c3b8a9df15
Md Rifajul Islam (Rifaj) <146623093+rifaj007@users.noreply.github.com>
2025-02-19 03:31:42 +0600

    basic application setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header />
+      <main className="font-inter text-">This is Layout page{children}</main>
+      <Footer />
+    </>
+  );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='font-poppins'>this is home page</div>
+  )
+}
+
+export default page
